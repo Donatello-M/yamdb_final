c674485fe169f9d21d3d5d08ccc6cc097806c54d
Danila Makarichev <72694196+Donatello-M@users.noreply.github.com>
2022-02-09 04:11:49 +0300

    Update settings.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY',
                        default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
-DEBUG = strtobool(os.getenv('DEBUG', default='False'))
+DEBUG = strtobool(os.getenv('DEBUG', default='True'))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split(', ')
 
